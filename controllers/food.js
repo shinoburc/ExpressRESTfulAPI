1e9d49a8e70cf7200d220a1b7e5c4c3846279489
shinoburc <dandydot@gmail.com>
2019-10-20 18:35:22 +0900

    add validator.

--- controllers/food.js
+++ controllers/food.js
@@ -1,18 +1,27 @@
 const Food = require('../models/food');
+const { check, validationResult } = require('express-validator');
 
 // CRUD RESTful API
 // Create
-exports.create = (req, res) => {
+exports.create = [[
+    check('name').isString(),
+    check('price').isNumeric()
+  ], (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
   const food = new Food(req.body);
 
   food.save(function(err){
     if(!err) {
       return res.json(food)
     } else {
-      return res.status(500).json({ message: 'user create faild.' });
+      return res.status(500).json({ message: 'food create faild.' });
     }
   });
-};
+}];
 
 // Read(find all)
 exports.findAll = (req, res) => {
@@ -49,7 +58,16 @@ exports.findOne = (req, res) => {
 };
 
 // Update
-exports.update = (req, res) => {
+exports.update = [[
+      check('name').isString(),
+      check('price').isNumeric()
+  ], (req, res) => {
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+
   Food.findByIdAndUpdate(req.params.id, {
     name: req.body.name || "No name", 
     price: req.body.price,
@@ -71,7 +89,7 @@ exports.update = (req, res) => {
       message: "Something wrong updating food with id " + req.params.id
     });
   });
-};
+}];
 
 // DELETE
 exports.delete = (req, res) => {
