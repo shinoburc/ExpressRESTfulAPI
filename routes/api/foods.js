6c9aa05c9acb14bae22543c82cc8554530f46ed2
shinoburc <dandydot@gmail.com>
2019-10-21 01:02:59 +0900

    move validation filter route to controller.

--- routes/api/foods.js
+++ routes/api/foods.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const model = require('../../models/food');
 const food = require('../../controllers/food');
 
 // CRUD RESTful API
-router.post('/', model.createValidation, food.create);
-router.get('/', model.findAllValidation, food.findAll);
-router.get('/:id', model.findOneValidation, food.findOne);
-router.put('/:id', model.updateValidation, food.update);
-router.delete('/:id', model.deleteValidation, food.delete);
+router.post('/', food.create);
+router.get('/',  food.findAll);
+router.get('/:id', food.findOne);
+router.put('/:id', food.update);
+router.delete('/:id', food.delete);
 
 module.exports = router;
